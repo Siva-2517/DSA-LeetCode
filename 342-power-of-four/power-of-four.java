a9e974ade36d6be1fce538f7cca44963dcae1d31
Siva Surya <sivasurya6430@gmail.com>
2026-07-17 09:45:00 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (99.21%) - LeetSync

--- 342-power-of-four/power-of-four.java
+++ 342-power-of-four/power-of-four.java
@@ -1,11 +1,5 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if(n <= 0) return false;
-        while(n > 1) {
-            if((n & 3) !=0)
-                return false;
-            n = n >> 2;
-        }
-        return true;
+        return (n & (n - 1)) == 0 && n % 3 == 1;
     }
 }
