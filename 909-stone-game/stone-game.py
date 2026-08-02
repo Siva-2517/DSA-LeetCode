ff1a871a5e3bdd118b9b011a01ab8e546c74b8b6
Siva Surya <sivasurya6430@gmail.com>
2026-08-02 16:11:16 +0530

    Time: 137 ms (37.70%) | Memory: 24.8 MB (42.43%) - LeetSync

--- 909-stone-game/stone-game.py
+++ 909-stone-game/stone-game.py
@@ -1,3 +1,16 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        return True
+        n = len(piles)
+        dp = [[0] * n for _ in range(n)]
+
+        for i in range(n):
+            dp[i][i] = piles[i]
+
+        for size in range(2, n + 1):
+            for i in range(n - size + 1):
+                j = i + size - 1
+                left = piles[i] - dp[i + 1][j]
+                right = piles[j] - dp[i][j - 1]
+                dp[i][j] = max(left, right)
+
+        return dp[0][n - 1] > 0
