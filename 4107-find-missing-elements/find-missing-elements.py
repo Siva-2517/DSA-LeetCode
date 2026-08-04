452168244c93d063e163cdf32af71cb342de748e
Siva Surya <sivasurya6430@gmail.com>
2026-08-04 23:28:17 +0530

    Time: 2 ms (75.11%) | Memory: 19.1 MB (97.28%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.py
+++ 4107-find-missing-elements/find-missing-elements.py
@@ -1,14 +1,6 @@
 class Solution:
     def findMissingElements(self, nums: List[int]) -> List[int]:
-        mn=min(nums)
-        mx=max(nums)
-        return [x for x in range(mn,mx+1) if x not in nums]   
-
-
-
-# class Solution:
-#     def findMissingElements(self, nums: List[int]) -> List[int]:
-#         st = set(nums)
-#         mn = min(nums)
-#         mx = max(nums)
-#         return [x for x in range(mn + 1, mx) if x not in st]
+        st = set(nums)
+        mn = min(nums)
+        mx = max(nums)
+        return [x for x in range(mn + 1, mx) if x not in st]
