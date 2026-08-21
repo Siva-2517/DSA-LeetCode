8769c50e5f2b9aa49545a046a2d3539fad4057d1
Siva Surya <sivasurya6430@gmail.com>
2026-08-21 22:46:08 +0530

    Time: 4 ms (39.41%) | Memory: 43.3 MB (45.79%) - LeetSync

--- 851-goat-latin/goat-latin.java
+++ 851-goat-latin/goat-latin.java
@@ -1,19 +1,18 @@
 class Solution {
     public String toGoatLatin(String s) {
         String[] a = s.split(" ");
-        String r = "";
+        StringBuilder r = new StringBuilder();
         String v = "aeiouAEIOU";
         for (int i = 0; i < a.length; i++) {
             String w = a[i];
             if (v.indexOf(w.charAt(0)) == -1)
                 w = w.substring(1) + w.charAt(0);
-            w += "ma";
+            r.append(w).append("ma");
             for (int j = 0; j <= i; j++)
-                w += "a";
-            if (i > 0)
-                r += " ";
-            r += w;
+                r.append("a");
+            if (i < a.length - 1)
+                r.append(" ");
         }
-        return r;
+        return r.toString();
     }
 }
